c7811803b23e626a38563918ca4fc81c7302ff34
Nole Snieckus <nole.snieckus@uconn.edu>
2023-03-12 14:46:41 -0400

    Updated Scene

--- ARcademic/Assets/Scripts/VideoButton.cs
+++ ARcademic/Assets/Scripts/VideoButton.cs
@@ -7,7 +7,7 @@ public class VideoButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Nothing
     }
 
     // Update is called once per frame
