553343206d1423a33fe3d77e87ae9c6f73d73084
Snieckus <nms18008@ad.uconn.edu>
2023-03-26 15:31:31 -0400

    Still Erroring
    just to upload

---
+++ ARcademic/Assets/Scripts/Tesseract/TesseractScipt.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TesseractDemoScript : MonoBehaviour
+{
+    [SerializeField] private Texture2D imageToRecognize;
+    [SerializeField] private Text displayText;
+    [SerializeField] private RawImage outputImage;
+    private TesseractDriver _tesseractDriver;
+    private string _text = "";
+    private Texture2D _texture;
+
+    private void Start()
+    {
+        Startup();
+    }
+
+    private void Startup()
+    {
+        Texture2D texture = new Texture2D(imageToRecognize.width, imageToRecognize.height, TextureFormat.ARGB32, false);
+        texture.SetPixels32(imageToRecognize.GetPixels32());
+        texture.Apply();
+
+        _tesseractDriver = new TesseractDriver();
+        Recoginze(texture);
+    }
+
+    private void Recoginze(Texture2D outputTexture)
+    {
+        _texture = outputTexture;
+        ClearTextDisplay();
+        AddToTextDisplay(_tesseractDriver.CheckTessVersion());
+        _tesseractDriver.Setup(OnSetupCompleteRecognize);
+    }
+
+    private void OnSetupCompleteRecognize()
+    {
+        AddToTextDisplay(_tesseractDriver.Recognize(_texture));
+        AddToTextDisplay(_tesseractDriver.GetErrorMessage(), true);
+        //    TODO ////
+        ///Change to UI ///
+        //SetImageDisplay();
+    }
+
+    private void ClearTextDisplay()
+    {
+        _text = "";
+    }
+
+    private void AddToTextDisplay(string text, bool isError = false)
+    {
+        ////    TODO /////
+        ////  Change for UI /////
+        if (string.IsNullOrWhiteSpace(text)) return;
+
+        _text += (string.IsNullOrWhiteSpace(displayText.text) ? "" : "\n") + text;
+
+        if (isError)
+            Debug.LogError(text);
+        else
+            Debug.Log(text);
+    }
+
+}
