bbdcd27bfd3897b6f5aeb1d643053ae17f34c27d
Connor Rickermann <connor.rickermann@gmail.com>
2023-04-09 13:27:27 -0400

    Update VoskResultText.cs
    IO output for Voice recognition, no write redundancy. Exports to single transposition file per day.

--- ARcademic/Assets/Scripts/Vosk Interface/VoskResultText.cs
+++ ARcademic/Assets/Scripts/Vosk Interface/VoskResultText.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Globalization;
+
 public class VoskResultText : MonoBehaviour 
 {
     public VoskSpeechToText VoskSpeechToText;
@@ -16,6 +20,20 @@ public class VoskResultText : MonoBehaviour
         ResultText.text = "Recognized: ";
         var result = new RecognitionResult(obj);
         ResultText.text += result.Phrases[0].Text + "\n" + "Confidence: " + result.Phrases[0].Confidence;
+
+        // Write output to file
+        DateTime mom = DateTime.Now;
+        string path = Application.persistentDataPath + string.Format("/transpose_{0}-{1}-{2}.txt", mom.Month, mom.Day, mom.Year);
+        DateTime lwt = Directory.GetLastWriteTime(path);
+
+        if ((mom.Hour != lwt.Hour) | (mom.Minute != lwt.Minute) | (mom.Second != lwt.Second))
+        {
+            Debug.Log("Writing to: " + path);
+            StreamWriter writer = new StreamWriter(path, true);
+            writer.WriteLine(result.Phrases[0].Text);
+            writer.Close();
+        }
+
         /*
         for (int i = 0; i < result.Phrases.Length; i++) {
             if (i > 0)
